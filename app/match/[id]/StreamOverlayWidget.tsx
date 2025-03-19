c88e50f2948a0c55e6c0e83724ce63bc55c07ffc
Wiktor Golicz <wiktorgolicz1@gmail.com>
2025-03-19 18:40:53 +0100

    Layout for overlay

---
+++ app/match/[id]/StreamOverlayWidget.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import useLiveMatch from "@/lib/hooks/useLiveMatch";
+import { Match } from "@/types/types";
+import MatchTimer from "@/components/MatchTimer";
+
+// styles
+import "./StreamOverlayWidget.scss";
+
+function StreamOverlayWidget({ initialMatch }: { initialMatch: Match }) {
+	const match = useLiveMatch(initialMatch);
+
+	return (
+		<>
+			<div className="counter-widget widget">
+				<span className="team-name">{match.team1}</span>
+				<span className="score">
+					{match.score1} : {match.score2}
+				</span>
+				<span className="team-name">{match.team2}</span>
+			</div>
+			<div className="timer-widget widget">
+				<MatchTimer match={match} />
+			</div>
+		</>
+	);
+}
+
+export default StreamOverlayWidget;
--- app/match/[id]/page.tsx
+++ app/match/[id]/page.tsx
@@ -1,40 +1,24 @@
 import { getMatch } from "@/app/actions/matchActions";
 import { notFound } from "next/navigation";
-import LiveMatchScore from "@/components/LiveMatchScore";
+import StreamOverlayWidget from "./StreamOverlayWidget";
 import Link from "next/link";
 
-export default async function MatchPage({ params }: { params: Promise<{ id: string }> }) {
-	const { id: matchId } = await params;
-	const match = await getMatch(matchId);
+// styles
+import "./StreamOverlay.scss";
 
-	if (!match) {
+export default async function StreamOverlay({ params }: { params: Promise<{ id: string }> }) {
+	const { id: matchId } = await params;
+	const initialMatch = await getMatch(matchId);
+	if (!initialMatch) {
 		notFound();
 	}
 
 	return (
-		<div className="max-w-3xl mx-auto py-8 px-4">
-			<h1 className="text-3xl font-bold text-center mb-8">
-				{match.team1} vs {match.team2}
-			</h1>
-			<h1 className="text-3xl font-bold text-center mb-8">
-				{match.score1} : {match.score2}
-			</h1>
-
-			<div className="bg-white rounded-lg shadow-lg overflow-hidden">
-				<div className="bg-green-700 text-white p-4 text-center">
-					<div className="text-sm">
-						{/* {new Date(match.date).toLocaleDateString()} | {new Date(match.date).toLocaleTimeString()} */}
-					</div>
-				</div>
-
-				<LiveMatchScore match={match} />
-
-				<div className="p-4 text-center">
-					<Link href="/" className="text-blue-600 hover:underline">
-						Back to all matches
-					</Link>
-				</div>
-			</div>
+		<div className="stream-overlay">
+			<StreamOverlayWidget initialMatch={initialMatch} />
+			<Link href="/" className="return-home">
+				Powrót do strony głównej
+			</Link>
 		</div>
 	);
 }
